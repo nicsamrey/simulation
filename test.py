765b48934445fcd370bd1f58b6699aa7256121ad
C <nicolesamrey@gmail.com>
2022-04-06 00:15:42 +0800

    initial simulation

--- test.py
+++ test.py
@@ -1,5 +1,37 @@
 
 
+from random import randint
 
 
 print("test")
+
+
+allocation = 0.
+ctr=0
+
+loop=0
+winCount = 0
+while loop < 1000:
+    maxLoop = 100
+    startval = 100
+    ctr = 0
+    while startval > 10 and maxLoop > ctr: 
+        bet = startval * allocation
+        cash = startval - bet
+        win = 0
+        #print('[' + str(ctr) + ']'+'start value: ' + str(startval) + ' bet: ' + str(bet), end=' ')
+        if randint(0,1):
+            #print('W', end=' ')
+            startval = cash + bet * 2
+        else:
+            #print('L', end=' ')
+            startval = cash + bet * 0.5
+        #print(' Bal:' + str(startval))
+        #print('')
+        ctr = ctr+1
+    loop = loop + 1
+    #print('[' + str(loop) + ']'+'maxLoop: ' + str(maxLoop)+' startval: ' + str(startval))
+    if startval > 200:
+        winCount = winCount + 1
+
+print('Summary Win Rate : '+ str(winCount/1000))
